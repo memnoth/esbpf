75b3249c4ba002a1e646e6c5f89ee60d3a0ff93d
Leesoo Ahn <lsahn@ooseel.net>
2021-05-01 17:22:29 +0900

    Init experiment.h file

---
+++ experiment.h
@@ -0,0 +1,15 @@
+/** experiment.h
+ *
+ * Copyright (c) 2021 Leesoo Ahn <lsahn@ooseel.net>
+ *
+ * Experimental features defined by esBPF
+ */
+
+#ifndef __ESBPF_EXPERIMENT_H__
+#define __ESBPF_EXPERIMENT_H__
+
+#if !defined(__ESBPF_CORE_H__)
+# error "Only <esbpf/core.h> can be included directly!"
+#endif
+
+#endif /* __ESBPF_EXPERIMENT_H__ */
